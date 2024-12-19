fa424753e60023d5d24b575c2c35c6e519f8f5ff
Passakorn Puttama <54370241+boss4848@users.noreply.github.com>
2024-12-19 21:01:18 +0700

    feat: add tutor page

---
+++ app/(tutor)/layout.tsx
@@ -0,0 +1,55 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/tutor/app-sidebar"
+import { NavActions } from "@/components/tutor/nav-actions"
+import { SidebarRight } from "@/components/tutor/sidebar-right"
+
+interface TutorLayoutProps {
+  children?: React.ReactNode
+}
+
+export default function TutorLayout({ children }: TutorLayoutProps) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="bg-background sticky top-0 flex h-14 shrink-0 items-center gap-2">
+          <div className="flex flex-1 items-center gap-2 px-3">
+            <SidebarTrigger />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">
+                    Building Your Application
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+          <div className="ml-auto px-3">
+            <NavActions />
+          </div>
+        </header>
+        {children}
+      </SidebarInset>
+      <SidebarRight />
+    </SidebarProvider>
+  )
+}
